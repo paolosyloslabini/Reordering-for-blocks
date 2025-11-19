4bd659229a87c351ff6c03cad2fbfca6983be57c
paolosyloslabini <paolosyloslabini@gmail.com>
2025-11-19 15:26:13 +0100

    parse test

--- scripts/parse_results.py
+++ scripts/parse_results.py
@@ -2,12 +2,13 @@
 
 from sbatchman import jobs_list
 
-job = jobs_list()[0]
-command, pos_args, named_args = job.parse_command_args()
-print(command)
-print(pos_args)
-print(named_args)
-print(job.status)
-print(job.variables)
-print(job.get_stdout())
+jobs = jobs_list(from_archived=True, tag = ["CuSPARSE_CSR_SPMM",], status = ["COMPLETED",])
+for job in jobs:
+    command, pos_args, named_args = job.parse_command_args()
+    print(command)
+    print(pos_args)
+    print(named_args)
+    print(job.status)
+    print(job.variables)
+    print(job.get_stdout())
 
